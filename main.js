153b728126e2e6c4020e830f7a8450eeb970064b
ITO Rina <218c0052@s.obirin.ac.jp>
2020-07-16 19:04:00 +0900

    日付

--- main.js
+++ main.js
@@ -29,4 +29,16 @@ socketio.on('message',function(msg){
   chats.append(dt);
   dd.append(obj.msg);
   chats.append(dd);
+  dd2.append(formatDate(new Date(msg.date), 'yyyy/MM/dd HH:mm:ss'));
 });
+
+function formatDate (date, format) {
+  format = format.replace(/yyyy/g, date.getFullYear());
+  format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
+  format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
+  format = format.replace(/HH/g, ('0' + date.getHours()).slice(-2));
+  format = format.replace(/mm/g, ('0' + date.getMinutes()).slice(-2));
+  format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
+  format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
+  return format;
+};
