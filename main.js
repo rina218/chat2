2b1654ed849185b9db36432d4a1c7e924b3c2a4d
ITO Rina <218c0052@s.obirin.ac.jp>
2020-07-20 09:39:05 +0900

    エラーが出たため修正

--- main.js
+++ main.js
@@ -8,31 +8,56 @@ const name = document.getElementById("name");
 
 let username='';
 nameform.addEventListener('submit', function(event){
+  if(name.value!==''){
   username = name.value;
-  event.preventDefault();
   nameform.style.display ="none";
   form.style.display ="block";
+
+  socketio.emit('signin');
+
+  const msg = {msg: username + ' さんが参加しました ', name: 'システム'};
+  socketio.emit('massage', msg);
+  }
+
+  event.preventDefault();
 })
 
 form.addEventListener('submit', function(event){
-  const msg = JSON.stringify({msg: input.value, name: username})
+  if(input.value!==''){
+    const msg = {msg: input.value, name: username};
   socketio.emit('message', msg);
   input.value='';
+  }
   event.preventDefault();
 })
-socketio.on('message',function(msg){
-  const obj = JSON.parse(msg);
 
-  function displayMessage(msg){
-    const dt = document.createElement("dt");
-    const dd = document.createElement("dd");
-    const dd2 = document.createElement("dd");
-    dt.append(msg.name);
-    chats.append(dt);
-    dd.append(msg.msg);
-    chats.append(dd);
-    dd2.append(formatDate(new Date(msg.date), 'yyyy/MM/dd HH:mm:ss'));
-    chats.append(dd2);
+socketio.on('message',function(msg){
+  if(username===''){
+    // まだ参加していなかったら、チャットを表示しない
+    return;
+  }
+  displayMessage(msg);
+});
+
+// 参加時に過去のメッセージを受け取る
+socketio.on('signin',function(msgs){
+  for(let i=0;i<msgs.length;i++){
+    const msg = msg[i];
+    displayMessage(msg);
+  }
+});
+
+function displayMessage(msg){
+  const dt = document.createElement("dt");
+  const dd = document.createElement("dd");
+  const dd2 = document.createElement("dd");
+  dt.append(msg.name);
+  chats.append(dt);
+  dd.append(msg.name);
+  chats.append(dd);
+  dd2.append(formatDate(new Date(msg.date), 'yyyy/MM/dd HH:mm:ss'));
+  chats.append(dd2);
+}
 
 function formatDate (date, format) {
   format = format.replace(/yyyy/g, date.getFullYear());
